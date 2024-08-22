73c5b10dca5bb873ef41648ffed61d61bbc9c857
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-08-22 12:47:51 -0300

    Fix minor bugs

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -11,12 +11,15 @@ export default function Frame({ id }) {
   const { config, models, frames } = useContext(ConfigContext)
 
   // console.log("Frame (id, frame.forecastTime)", id, frame.forecastTime)
-
+  // console.log("id", id)
+  // console.log("frames", frames)
   const [frame, setFrame] = useState(frames.find(item => item.id === id))
+  // console.log("frames.find(item => item.id === id)", frames.find(item => item.id === id))
+  // console.log("frame", frame)
   const [model, setModel] = useState(models.find(model => model.value === frame.model))
+  // console.log("model", model)
 
   // console.log("frame.model", frame.model)
-  // console.log("frame", frame)
   // console.log("models", models)
 
   // console.log("model", model)
