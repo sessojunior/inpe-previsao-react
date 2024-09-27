9da801228de347968243066b4823bba04b56286d
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-09-27 14:47:35 -0300

    Updated footer and header styles

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,95 +1,218 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 
-import { ConfigContext } from '../contexts/ConfigContext'
+import { ConfigContext } from "../contexts/ConfigContext";
 
 import { FaCookieBite, FaXTwitter } from "react-icons/fa6";
 import { FaYoutube, FaFacebook, FaInstagramSquare } from "react-icons/fa";
 
-import AcessoInformacao from "../assets/acesso-informacao.svg"
-import imgLogoWhite from "../assets/govbr-logo-white.png"
+import AcessoInformacao from "../assets/acesso-informacao.svg";
+import imgLogoWhite from "../assets/govbr-logo-white.png";
 
 export default function Footer() {
-
-  const { config } = useContext(ConfigContext)
+  const { config } = useContext(ConfigContext);
 
   const linksAcesso = [
-    { label: 'Institucional', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/institucional' },
-    { label: 'Ações e Programas', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/acoes-e-programas' },
-    { label: 'Participação Social', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/participacao-social' },
-    { label: 'Auditorias', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/auditorias' },
-    { label: 'Convênios e Transferências', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/convenios-e-transferencias' },
-    { label: 'Receitas e Despesas', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/receitas-e-despesas' },
-    { label: 'Licitações e Contratos', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/licitacoes-e-contratos' },
-    { label: 'Servidores', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/servidores' },
-    { label: 'Informações Classificadas', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/informacoes-classificadas' },
-    { label: 'Serviço de Informação ao Cidadão - SIC', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/servico-de-informacao-ao-cidadao-sic' },
-    { label: 'Perguntas Frequentes', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/perguntas-frequentes' },
-    { label: 'Dados Abertos', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/dados-abertos' },
-    { label: 'Transparência e Prestação de Contas', url: 'https://www.gov.br/inpe/pt-br/acesso-a-informacao/transparencia-e-prestacao-de-contas' },
-  ]
+    {
+      label: "Institucional",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/institucional",
+    },
+    {
+      label: "Ações e Programas",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/acoes-e-programas",
+    },
+    {
+      label: "Participação Social",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/participacao-social",
+    },
+    {
+      label: "Auditorias",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/auditorias",
+    },
+    {
+      label: "Convênios e Transferências",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/convenios-e-transferencias",
+    },
+    {
+      label: "Receitas e Despesas",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/receitas-e-despesas",
+    },
+    {
+      label: "Licitações e Contratos",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/licitacoes-e-contratos",
+    },
+    {
+      label: "Servidores",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/servidores",
+    },
+    {
+      label: "Informações Classificadas",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/informacoes-classificadas",
+    },
+    {
+      label: "Serviço de Informação ao Cidadão - SIC",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/servico-de-informacao-ao-cidadao-sic",
+    },
+    {
+      label: "Perguntas Frequentes",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/perguntas-frequentes",
+    },
+    {
+      label: "Dados Abertos",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/dados-abertos",
+    },
+    {
+      label: "Transparência e Prestação de Contas",
+      url: "https://www.gov.br/inpe/pt-br/acesso-a-informacao/transparencia-e-prestacao-de-contas",
+    },
+  ];
 
   const linksAssuntos = [
-    { label: 'Últimas Notícias', url: 'https://www.gov.br/inpe/pt-br/assuntos/ultimas-noticias' },
-    { label: 'Produtos', url: 'https://www.gov.br/inpe/pt-br/assuntos/produtos' },
-    { label: 'Eventos', url: 'https://www.gov.br/inpe/pt-br/assuntos/eventos' },
-    { label: 'Capacitação Pós-Graduação', url: 'https://www.gov.br/inpe/pt-br/area-conhecimento/posgraduacao/' },
-    { label: 'Anúncio de Oportunidades', url: 'https://www.gov.br/inpe/pt-br/assuntos/anuncio-de-oportunidades' },
-    { label: 'Atuação do INPE no Desastre do RS', url: 'https://www.gov.br/inpe/pt-br/assuntos/atuacao-do-inpe-no-desastre-no-rs' },
-  ]
+    {
+      label: "Últimas Notícias",
+      url: "https://www.gov.br/inpe/pt-br/assuntos/ultimas-noticias",
+    },
+    {
+      label: "Produtos",
+      url: "https://www.gov.br/inpe/pt-br/assuntos/produtos",
+    },
+    { label: "Eventos", url: "https://www.gov.br/inpe/pt-br/assuntos/eventos" },
+    {
+      label: "Capacitação Pós-Graduação",
+      url: "https://www.gov.br/inpe/pt-br/area-conhecimento/posgraduacao/",
+    },
+    {
+      label: "Anúncio de Oportunidades",
+      url: "https://www.gov.br/inpe/pt-br/assuntos/anuncio-de-oportunidades",
+    },
+    {
+      label: "Atuação do INPE no Desastre do RS",
+      url: "https://www.gov.br/inpe/pt-br/assuntos/atuacao-do-inpe-no-desastre-no-rs",
+    },
+  ];
 
   const linksCentral = [
-    { label: 'Cartilhas Educacionais', url: 'https://www.gov.br/inpe/pt-br/central-de-conteudo/cartilhas-educacionais' },
-    { label: 'Publicações', url: 'https://www.gov.br/inpe/pt-br/central-de-conteudo/publicacoes' },
-    { label: 'Biblioteca On-Line', url: 'https://www.gov.br/inpe/pt-br/area-conhecimento/biblioteca' },
-    { label: 'Vídeos', url: 'http://www.youtube.com/inpemct' },
-    { label: 'Área de Conhecimento', url: 'https://www.gov.br/inpe/pt-br/area-conhecimento' },
-    { label: 'Vídeos Institucionais', url: 'https://www.gov.br/inpe/pt-br/central-de-conteudo/videos-institucionais' },
-  ]
+    {
+      label: "Cartilhas Educacionais",
+      url: "https://www.gov.br/inpe/pt-br/central-de-conteudo/cartilhas-educacionais",
+    },
+    {
+      label: "Publicações",
+      url: "https://www.gov.br/inpe/pt-br/central-de-conteudo/publicacoes",
+    },
+    {
+      label: "Biblioteca On-Line",
+      url: "https://www.gov.br/inpe/pt-br/area-conhecimento/biblioteca",
+    },
+    { label: "Vídeos", url: "http://www.youtube.com/inpemct" },
+    {
+      label: "Área de Conhecimento",
+      url: "https://www.gov.br/inpe/pt-br/area-conhecimento",
+    },
+    {
+      label: "Vídeos Institucionais",
+      url: "https://www.gov.br/inpe/pt-br/central-de-conteudo/videos-institucionais",
+    },
+  ];
 
   const linksCanais = [
-    { label: 'Ouvidoria', url: 'https://www.gov.br/inpe/pt-br/canais_atendimento/ouvidoria' },
-    { label: 'Imprensa', url: 'https://www.gov.br/inpe/pt-br/canais_atendimento/imprensa' },
-    { label: 'Visitas ao INPE', url: 'http://www.gov.br/inpe/pt-br/acesso-a-informacao/institucional/visita/' },
-  ]
+    {
+      label: "Ouvidoria",
+      url: "https://www.gov.br/inpe/pt-br/canais_atendimento/ouvidoria",
+    },
+    {
+      label: "Imprensa",
+      url: "https://www.gov.br/inpe/pt-br/canais_atendimento/imprensa",
+    },
+    {
+      label: "Visitas ao INPE",
+      url: "http://www.gov.br/inpe/pt-br/acesso-a-informacao/institucional/visita/",
+    },
+  ];
 
   return (
     <>
-      {config.showHeaderFooter &&
+      {config.showHeaderFooter && (
         <footer className="w-full flex flex-col bg-blue-900">
           <div className="bg-blue-900 w-full flex flex-col px-4 pt-4 pb-8 xl:max-w-[1280px] xl:mx-auto">
             <div className="my-6 lg:my-8">
-              <img src={imgLogoWhite} alt="Governo Federal" className="h-10 lg:h-14" />
+              <img
+                src={imgLogoWhite}
+                alt="Governo Federal"
+                className="h-10 lg:h-14"
+              />
             </div>
             <div className="grid lg:grid-cols-4 gap-4">
               <div className="pb-4">
-                <h2 className="text-lg text-white font-medium uppercase pb-4"><a href="https://www.gov.br/inpe/pt-br/acesso-a-informacao">Acesso à informação</a></h2>
-                <ul>
+                <h2 className="text-lg text-white font-medium uppercase pb-4">
+                  <a href="https://www.gov.br/inpe/pt-br/acesso-a-informacao">
+                    Acesso à informação
+                  </a>
+                </h2>
+                <ul className="leading-7">
                   {linksAcesso.map((link, index) => (
-                    <li key={index}><a href={link.url} className="text-white hover:text-gray-300">{link.label}</a></li>
+                    <li key={index}>
+                      <a
+                        href={link.url}
+                        className="text-white hover:text-gray-300"
+                      >
+                        {link.label}
+                      </a>
+                    </li>
                   ))}
                 </ul>
               </div>
               <div className="pb-4">
-                <h2 className="text-lg text-white font-medium uppercase pb-4"><a href="https://www.gov.br/inpe/pt-br/assuntos">Assuntos</a></h2>
-                <ul>
+                <h2 className="text-lg text-white font-medium uppercase pb-4">
+                  <a href="https://www.gov.br/inpe/pt-br/assuntos">Assuntos</a>
+                </h2>
+                <ul className="leading-7">
                   {linksAssuntos.map((link, index) => (
-                    <li key={index}><a href={link.url} className="text-white hover:text-gray-300">{link.label}</a></li>
+                    <li key={index}>
+                      <a
+                        href={link.url}
+                        className="text-white hover:text-gray-300"
+                      >
+                        {link.label}
+                      </a>
+                    </li>
                   ))}
                 </ul>
               </div>
               <div className="pb-4">
-                <h2 className="text-lg text-white font-medium uppercase pb-4"><a href="https://www.gov.br/inpe/pt-br/central-de-conteudo">Central de conteúdo</a></h2>
-                <ul>
+                <h2 className="text-lg text-white font-medium uppercase pb-4">
+                  <a href="https://www.gov.br/inpe/pt-br/central-de-conteudo">
+                    Central de conteúdo
+                  </a>
+                </h2>
+                <ul className="leading-7">
                   {linksCentral.map((link, index) => (
-                    <li key={index}><a href={link.url} className="text-white hover:text-gray-300">{link.label}</a></li>
+                    <li key={index}>
+                      <a
+                        href={link.url}
+                        className="text-white hover:text-gray-300"
+                      >
+                        {link.label}
+                      </a>
+                    </li>
                   ))}
                 </ul>
               </div>
               <div className="pb-8">
-                <h2 className="text-lg text-white font-medium uppercase pb-4"><a href="https://www.gov.br/inpe/pt-br/canais_atendimento">Canais de atendimento</a></h2>
-                <ul>
+                <h2 className="text-lg text-white font-medium uppercase pb-4">
+                  <a href="https://www.gov.br/inpe/pt-br/canais_atendimento">
+                    Canais de atendimento
+                  </a>
+                </h2>
+                <ul className="leading-7">
                   {linksCanais.map((link, index) => (
-                    <li key={index}><a href={link.url} className="text-white hover:text-gray-300">{link.label}</a></li>
+                    <li key={index}>
+                      <a
+                        href={link.url}
+                        className="text-white hover:text-gray-300"
+                      >
+                        {link.label}
+                      </a>
+                    </li>
                   ))}
                 </ul>
               </div>
@@ -99,12 +222,42 @@ export default function Footer() {
             </div> */}
             <div className="py-4 flex justify-between">
               <div>
-                <h2 className="text-lg text-white font-medium uppercase pb-4">Redes sociais</h2>
+                <h2 className="text-lg text-white font-medium uppercase pb-4">
+                  Redes sociais
+                </h2>
                 <ul>
-                  <li className="inline-block pr-4"><a href="https://www.twitter.com/inpe_mcti" className="text-white hover:text-gray-300"><FaXTwitter /></a></li>
-                  <li className="inline-block pr-4"><a href="https://www.youtube.com/inpemct" className="text-white hover:text-gray-300"><FaYoutube /></a></li>
-                  <li className="inline-block pr-4"><a href="https://www.facebook.com/inpe.oficial" className="text-white hover:text-gray-300"><FaFacebook /></a></li>
-                  <li className="inline-block pr-4"><a href="https://www.instagram.com/inpe.oficial" className="text-white hover:text-gray-300"><FaInstagramSquare /></a></li>
+                  <li className="inline-block pr-4">
+                    <a
+                      href="https://www.twitter.com/inpe_mcti"
+                      className="text-white hover:text-gray-300"
+                    >
+                      <FaXTwitter />
+                    </a>
+                  </li>
+                  <li className="inline-block pr-4">
+                    <a
+                      href="https://www.youtube.com/inpemct"
+                      className="text-white hover:text-gray-300"
+                    >
+                      <FaYoutube />
+                    </a>
+                  </li>
+                  <li className="inline-block pr-4">
+                    <a
+                      href="https://www.facebook.com/inpe.oficial"
+                      className="text-white hover:text-gray-300"
+                    >
+                      <FaFacebook />
+                    </a>
+                  </li>
+                  <li className="inline-block pr-4">
+                    <a
+                      href="https://www.instagram.com/inpe.oficial"
+                      className="text-white hover:text-gray-300"
+                    >
+                      <FaInstagramSquare />
+                    </a>
+                  </li>
                 </ul>
               </div>
               <div className="flex items-center">
@@ -115,10 +268,19 @@ export default function Footer() {
             </div>
           </div>
           <div className="flex bg-blue-950 justify-center">
-            <p className="text-white text-center p-4">Todo o conteúdo deste site está publicado sob a licença <a href="https://creativecommons.org/licenses/by-nd/3.0/deed.pt_BR" className="font-bold hover:text-blue-400">Creative Commons Atribuição-SemDerivações 3.0 Não Adaptada</a>.</p>
+            <p className="text-white text-center p-4">
+              Todo o conteúdo deste site está publicado sob a licença{" "}
+              <a
+                href="https://creativecommons.org/licenses/by-nd/3.0/deed.pt_BR"
+                className="font-bold hover:text-blue-400"
+              >
+                Creative Commons Atribuição-SemDerivações 3.0 Não Adaptada
+              </a>
+              .
+            </p>
           </div>
         </footer>
-      }
+      )}
     </>
-  )
+  );
 }
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,45 +1,69 @@
-import { useContext } from 'react'
-import { ConfigContext } from '../contexts/ConfigContext'
+import { useContext } from "react";
+import { ConfigContext } from "../contexts/ConfigContext";
 
-import imgLogo from "../assets/govbr-logo.png"
+import imgLogo from "../assets/govbr-logo.png";
 
 export default function Header() {
-
-  const { config } = useContext(ConfigContext)
+  const { config } = useContext(ConfigContext);
 
   const linksTop = [
-    { label: 'Órgãos do governo', url: 'https://www.gov.br/pt-br/orgaos-do-governo' },
-    { label: 'Acesso à Informação', url: 'https://www.gov.br/acessoainformacao/pt-br' },
-    { label: 'Legislação', url: 'http://www4.planalto.gov.br/legislacao' },
-    { label: 'Acessibilidade', url: 'https://www.gov.br/governodigital/pt-br/acessibilidade-digital' },
-  ]
+    {
+      label: "Órgãos do governo",
+      url: "https://www.gov.br/pt-br/orgaos-do-governo",
+    },
+    {
+      label: "Acesso à Informação",
+      url: "https://www.gov.br/acessoainformacao/pt-br",
+    },
+    { label: "Legislação", url: "http://www4.planalto.gov.br/legislacao" },
+    {
+      label: "Acessibilidade",
+      url: "https://www.gov.br/governodigital/pt-br/acessibilidade-digital",
+    },
+  ];
 
   const linksBottom = [
-    { label: 'Tempo', url: 'http://tempo.cptec.inpe.br' },
-    { label: 'Clima', url: 'http://clima.cptec.inpe.br' },
-    { label: 'Satélite', url: 'http://satelite.cptec.inpe.br/home/novoSite/index.jsp' },
-    { label: 'Ondas', url: 'http://ondas.cptec.inpe.br' },
-    { label: 'Bacias', url: 'https://bacias.cptec.inpe.br' },
-    { label: 'Qualidade do Ar', url: 'http://meioambiente.cptec.inpe.br' },
-    { label: 'Pós Graduação', url: 'http://www.inpe.br/posgraduacao/met/' },
-  ]
+    { label: "Tempo", url: "http://tempo.cptec.inpe.br" },
+    { label: "Clima", url: "http://clima.cptec.inpe.br" },
+    {
+      label: "Satélite",
+      url: "http://satelite.cptec.inpe.br/home/novoSite/index.jsp",
+    },
+    { label: "Ondas", url: "http://ondas.cptec.inpe.br" },
+    { label: "Bacias", url: "https://bacias.cptec.inpe.br" },
+    { label: "Qualidade do Ar", url: "http://meioambiente.cptec.inpe.br" },
+    { label: "Pós Graduação", url: "http://www.inpe.br/posgraduacao/met/" },
+  ];
 
   return (
     <>
-      {config.showHeaderFooter &&
+      {config.showHeaderFooter && (
         <header className="bg-white w-full px-4 py-4 xl:max-w-[1280px] xl:mx-auto">
           <div className="flex justify-between mb-3 h-10">
-              <div className="flex items-center">
-                <div><a href="https://www.gov.br/pt-br"><img src={imgLogo} alt="Logo" /></a></div>
-                <div className="flex items-center">
-                  <span className="pl-2 ml-2 border-l-2 border-l-gray-400 text-md">Ministério da Ciência, Tecnologia e Inovação</span>
-                </div>
+            <div className="flex items-center">
+              <div>
+                <a href="https://www.gov.br/pt-br">
+                  <img src={imgLogo} alt="Logo" />
+                </a>
               </div>
+              <div className="flex items-center">
+                <span className="pl-2 ml-2 border-l-2 border-l-gray-400 text-md">
+                  Ministério da Ciência, Tecnologia e Inovação
+                </span>
+              </div>
+            </div>
             <div className="flex items-center text-right">
               <nav className="hidden lg:flex">
                 <ul>
                   {linksTop.map((link, index) => (
-                    <li key={index} className="inline-block pl-4"><a href={link.url} className="text-blue-600 hover:text-blue-800">{link.label}</a></li>
+                    <li key={index} className="inline-block pl-4">
+                      <a
+                        href={link.url}
+                        className="text-blue-600 hover:text-blue-800"
+                      >
+                        {link.label}
+                      </a>
+                    </li>
                   ))}
                 </ul>
               </nav>
@@ -47,7 +71,9 @@ export default function Header() {
           </div>
           <div className="flex justify-between mb-3">
             <div className="flex flex-col">
-              <div className="text-xl font-medium">Centro de Previsão de Tempo e Estudos Climáticos</div>
+              <div className="text-xl md:text-2xl font-medium">
+                Centro de Previsão de Tempo e Estudos Climáticos
+              </div>
               <div className="text-xl hidden md:flex">CPTEC</div>
             </div>
           </div>
@@ -55,13 +81,20 @@ export default function Header() {
             <nav>
               <ul>
                 {linksBottom.map((link, index) => (
-                  <li key={index} className="inline-block pr-4"><a href={link.url} className="text-blue-600 hover:text-blue-800">{link.label}</a></li>
+                  <li key={index} className="inline-block pr-4">
+                    <a
+                      href={link.url}
+                      className="text-blue-600 hover:text-blue-800"
+                    >
+                      {link.label}
+                    </a>
+                  </li>
                 ))}
               </ul>
             </nav>
           </div>
         </header>
-      }
+      )}
     </>
-  )
+  );
 }
