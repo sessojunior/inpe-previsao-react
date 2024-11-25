f20abd8ac97f778eae4f8faaf138defe0de44655
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-11-25 17:18:51 -0300

    Updated footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,15 +1,21 @@
-import { useContext } from "react";
-
+import { useContext, useState, useEffect } from "react";
 import { ConfigContext } from "../contexts/ConfigContext";
 
-import { FaCookieBite, FaXTwitter } from "react-icons/fa6";
-import { FaYoutube, FaFacebook, FaInstagramSquare } from "react-icons/fa";
+import {
+  FaYoutube,
+  FaFacebookF,
+  FaInstagram,
+  FaChevronDown,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
-import AcessoInformacao from "../assets/acesso-informacao.svg";
+import AcessoInformacao from "../assets/acesso-informacao.png";
 import imgLogoWhite from "../assets/govbr-logo-white.png";
 
 export default function Footer() {
   const { config } = useContext(ConfigContext);
+  const [activeSection, setActiveSection] = useState(null);
+  const [activeLinks, setActiveLinks] = useState(null);
 
   const linksAcesso = [
     {
@@ -129,154 +135,188 @@ export default function Footer() {
     },
   ];
 
+  const linksSocial = [
+    {
+      label: "Facebook",
+      url: "https://www.facebook.com/inpe.oficial",
+      icon: FaFacebookF,
+    },
+    {
+      label: "Instagram",
+      url: "https://www.instagram.com/cptecinpe/",
+      icon: FaInstagram,
+    },
+    {
+      label: "Twitter",
+      url: "https://x.com/inpe_mct",
+      icon: FaXTwitter,
+    },
+    {
+      label: "YouTube",
+      url: "https://www.youtube.com/inpemct",
+      icon: FaYoutube,
+    },
+  ];
+
+  useEffect(() => {
+    setActiveSection("acesso");
+    setActiveLinks(linksAcesso);
+  }, []);
+
+  const toggleSection = (section, links) => {
+    setActiveSection(activeSection === section ? null : section);
+    setActiveLinks(links);
+  };
+
+  const TitleMenuSection = ({ section, label, links }) => {
+    return (
+      <div className="inline-block mr-2 my-2 border-b border-[#6B788E]">
+        <div
+          className={`flex items-center justify-between cursor-pointer p-2 text-white hover:bg-[#263A5D] rounded ${
+            activeSection === section ? "bg-[#263A5D]" : ""
+          }`}
+          onClick={() => toggleSection(section, links)}
+        >
+          <h2 className="text-lg font-medium">{label}</h2>
+          <FaChevronDown
+            size={12}
+            className={`ml-2 transition-transform duration-300 ${
+              activeSection === section ? "transform rotate-180" : ""
+            }`}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const TitleLinkSection = ({ label, url }) => {
+    return (
+      <div className="inline-block mr-2 border-b border-[#6B788E]">
+        <div className="flex items-center justify-between cursor-pointer p-2 text-white hover:bg-[#263A5D] rounded">
+          <h2 className="text-lg font-medium">
+            <a href={url}>{label}</a>
+          </h2>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       {config.showHeaderFooter && (
-        <footer className="w-full flex flex-col bg-blue-900">
-          <div className="bg-blue-900 w-full flex flex-col px-4 pt-4 pb-8 xl:max-w-[1280px] xl:mx-auto">
+        <footer className="w-full flex flex-col bg-[#071D41]">
+          <div className="w-full flex flex-col px-4 pt-4 pb-8 xl:max-w-[1280px] xl:mx-auto">
             <div className="my-6 lg:my-8">
               <img
                 src={imgLogoWhite}
                 alt="Governo Federal"
-                className="h-10 lg:h-14"
+                className="h-10 lg:h-12"
               />
             </div>
-            <div className="grid lg:grid-cols-4 gap-4">
-              <div className="pb-4">
-                <h2 className="text-lg text-white font-medium uppercase pb-4">
-                  <a href="https://www.gov.br/inpe/pt-br/acesso-a-informacao">
-                    Acesso à informação
-                  </a>
-                </h2>
-                <ul className="leading-7">
-                  {linksAcesso.map((link, index) => (
-                    <li key={index}>
-                      <a
-                        href={link.url}
-                        className="text-white hover:text-gray-300"
-                      >
-                        {link.label}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="pb-4">
-                <h2 className="text-lg text-white font-medium uppercase pb-4">
-                  <a href="https://www.gov.br/inpe/pt-br/assuntos">Assuntos</a>
-                </h2>
-                <ul className="leading-7">
-                  {linksAssuntos.map((link, index) => (
-                    <li key={index}>
-                      <a
-                        href={link.url}
-                        className="text-white hover:text-gray-300"
-                      >
-                        {link.label}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="pb-4">
-                <h2 className="text-lg text-white font-medium uppercase pb-4">
-                  <a href="https://www.gov.br/inpe/pt-br/central-de-conteudo">
-                    Central de conteúdo
-                  </a>
-                </h2>
-                <ul className="leading-7">
-                  {linksCentral.map((link, index) => (
-                    <li key={index}>
-                      <a
-                        href={link.url}
-                        className="text-white hover:text-gray-300"
-                      >
-                        {link.label}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="pb-8">
-                <h2 className="text-lg text-white font-medium uppercase pb-4">
-                  <a href="https://www.gov.br/inpe/pt-br/canais_atendimento">
-                    Canais de atendimento
-                  </a>
-                </h2>
-                <ul className="leading-7">
-                  {linksCanais.map((link, index) => (
-                    <li key={index}>
-                      <a
-                        href={link.url}
-                        className="text-white hover:text-gray-300"
-                      >
-                        {link.label}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
+            <div className="w-full">
+              {/* Seção Acesso à Informação */}
+              <TitleMenuSection
+                section="acesso"
+                label="Acesso à Informação"
+                links={linksAcesso}
+              />
+
+              {/* Seção Assuntos */}
+              <TitleMenuSection
+                section="assuntos"
+                label="Assuntos"
+                links={linksAssuntos}
+              />
+
+              {/* Seção Central de Conteúdo */}
+              <TitleMenuSection
+                section="central"
+                label="Central de Conteúdo"
+                links={linksCentral}
+              />
+
+              {/* Seção Canais de atendimento */}
+              <TitleMenuSection
+                section="canais"
+                label="Canais de atendimento"
+                links={linksCanais}
+              />
+
+              {/* Seção Composição */}
+              <TitleLinkSection
+                url="https://www.gov.br/inpe/pt-br/composicao"
+                label="Composição"
+              />
+
+              {/* Seção Serviços */}
+              <TitleLinkSection
+                url="https://www.gov.br/inpe/pt-br/servicos"
+                label="Serviços"
+              />
+
+              <div>
+                {activeLinks && (
+                  <ul className="my-6">
+                    {activeLinks.map((link, index) => (
+                      <li key={index}>
+                        <a
+                          href={link.url}
+                          className="text-[#c5d4eb] hover:bg-[#263A5D] w-full block px-4 py-2 rounded"
+                        >
+                          {link.label}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
             </div>
             {/* <div className="flex flex-col py-4">
               <a href="#" className="text-white flex items-center"><span className="pr-2"><FaCookieBite /></span> Redefinir Cookies</a>
             </div> */}
             <div className="py-4 flex justify-between">
-              <div>
-                <h2 className="text-lg text-white font-medium uppercase pb-4">
+              <div className="w-full">
+                <h2 className="text-lg text-white font-medium uppercase pb-6">
                   Redes sociais
                 </h2>
-                <ul>
-                  <li className="inline-block pr-4">
+                <div className="flex items-center justify-between w-full">
+                  <ul>
+                    {linksSocial.map((link, index) => (
+                      <li key={index} className="inline-block pr-2">
+                        <a
+                          href={link.url}
+                          className="text-white hover:bg-[#405474] text-2xl w-10 h-10 flex items-center justify-center rounded-full"
+                          aria-label={link.label}
+                        >
+                          <link.icon size={20} />
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="flex items-center">
                     <a
-                      href="https://www.twitter.com/inpe_mcti"
-                      className="text-white hover:text-gray-300"
+                      href="https://www.gov.br/acessoainformacao/pt-br"
+                      className="flex items-center justify-center px-4 py-2 rounded-full hover:bg-[#405473]"
                     >
-                      <FaXTwitter />
+                      <img src={AcessoInformacao} alt="" className="w-8" />
+                      <span className="text-white ml-2">
+                        Acesso à informação
+                      </span>
                     </a>
-                  </li>
-                  <li className="inline-block pr-4">
-                    <a
-                      href="https://www.youtube.com/inpemct"
-                      className="text-white hover:text-gray-300"
-                    >
-                      <FaYoutube />
-                    </a>
-                  </li>
-                  <li className="inline-block pr-4">
-                    <a
-                      href="https://www.facebook.com/inpe.oficial"
-                      className="text-white hover:text-gray-300"
-                    >
-                      <FaFacebook />
-                    </a>
-                  </li>
-                  <li className="inline-block pr-4">
-                    <a
-                      href="https://www.instagram.com/inpe.oficial"
-                      className="text-white hover:text-gray-300"
-                    >
-                      <FaInstagramSquare />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="flex items-center">
-                <a href="https://www.gov.br/acessoainformacao/pt-br">
-                  <img src={AcessoInformacao} alt="" className="" />
-                </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex bg-blue-950 justify-center">
-            <p className="text-white text-center p-4">
-              Todo o conteúdo deste site está publicado sob a licença{" "}
+          <div className="flex justify-center border-t border-[#7E8A9E]">
+            <p className="text-white text-center text-sm p-4">
+              <span className="mr-2">Desenvolvido por:</span>
               <a
-                href="https://creativecommons.org/licenses/by-nd/3.0/deed.pt_BR"
-                className="font-bold hover:text-blue-400"
+                href="https://www.cptec.inpe.br/"
+                className="font-bold text-xs uppercase hover:bg-[#405474] py-1 px-2 rounded-full"
               >
-                Creative Commons Atribuição-SemDerivações 3.0 Não Adaptada
+                CPTEC
               </a>
-              .
             </p>
           </div>
         </footer>
