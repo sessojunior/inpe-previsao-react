af93f076d5f5f9275be88d0047fa4ae9efc704ed
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-11-25 12:33:04 -0300

    Fix date function

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -12,16 +12,19 @@ export function formatDate(date) {
   const day = date.slice(8, 10);
   const turn = date.slice(11, 13);
 
-  const fullDate = new Date(`${year}-${month}-${day}`);
+  // Criar um objeto Date em UTC
+  const fullDate = new Date(`${year}-${month}-${day}T${turn}:00:00Z`);
 
+  // Obter o nome do dia da semana (abreviado e capitalizado)
   const weekNameLowercase = fullDate
-    .toLocaleDateString("pt-BR", { weekday: "short" })
+    .toLocaleDateString("pt-BR", { weekday: "short", timeZone: "UTC" })
     .replace(".", "");
   const weekName =
     weekNameLowercase.charAt(0).toUpperCase() + weekNameLowercase.slice(1);
 
+  // Obter o nome do mês (abreviado e capitalizado)
   const monthNameLowercase = fullDate
-    .toLocaleDateString("pt-BR", { month: "short" })
+    .toLocaleDateString("pt-BR", { month: "short", timeZone: "UTC" })
     .replace(".", "");
   const monthName =
     monthNameLowercase.charAt(0).toUpperCase() + monthNameLowercase.slice(1);
